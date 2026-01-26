2480553f54483a46ddbd11a713b2fca81bcd33fc
E_Wick <59318611+ericChadick@users.noreply.github.com>
2026-01-26 16:40:16 -0500

    game

---
+++ Player/projectile.gd
@@ -0,0 +1,45 @@
+extends Area3D
+
+var speed : float = 80.0;
+var direction : Vector3 = Vector3.ZERO;
+var creator : Node3D;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	speed = creator.cordLength*8.0;
+	
+	position += direction * speed * delta;
+	
+	if global_position.distance_to(creator.global_position) > creator.cordLength:
+		destroy();
+
+func _on_body_entered(body: Node3D) -> void:
+	if body != creator:
+		destroy();
+	#if body.is_in_group("enemy"):
+		#body.hp -= 1;
+		##body.hit_particles.emitting = true;
+		#body.hit_sound.play();
+		#if body.hp <= 0:
+			#body.queue_free();
+		#queue_free();
+	
+
+#timer node connected signal to destroy self on timeout
+func _on_timer_timeout() -> void:
+	destroy();
+
+
+func _on_area_entered(area: Area3D) -> void:
+	if area.is_in_group("outlet"):
+		creator.outlet = area;
+		destroy();
+		
+func destroy() -> void:
+	creator.cordProjectile = null;
+	queue_free();
