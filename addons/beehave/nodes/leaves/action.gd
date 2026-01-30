2ebe282b6632a3263a1c6af38eb1f639e8a5d4e6
Wyatt <wyattschmidle@gmail.com>
2026-01-30 17:17:41 -0500

    fixed add on errors

--- addons/beehave/nodes/leaves/action.gd
+++ addons/beehave/nodes/leaves/action.gd
@@ -1,32 +1,13 @@
+## Actions are leaf nodes that define a task to be performed by an actor.
+## Their execution can be long running, potentially being called across multiple
+## frame executions. In this case, the node should return `RUNNING` until the
+## action is completed.
 @tool
-class_name CalculateMoveDirection_BH
-extends ActionLeaf
+@icon("../../icons/action.svg")
+class_name ActionLeaf extends Leaf
 
-enum MoveType {
-	TO_TARGET = 0,
-	AWAY_FROM_TARGET = 1,
-}
 
-@export var move_type: MoveType = MoveType.TO_TARGET
-
-func tick(actor: Node, _blackboard: Blackboard) -> int:
-	var enemy_actor := actor as Node3D
-	if enemy_actor == null:
-		return FAILURE
-
-	var target := get_tree().get_first_node_in_group("player") as Node3D
-	if target == null:
-		return FAILURE
-
-	match move_type:
-		MoveType.AWAY_FROM_TARGET:
-			# direction from target -> enemy (so enemy moves away)
-			enemy_actor.move_direction = target.global_position.direction_to(enemy_actor.global_position)
-			return SUCCESS
-
-		MoveType.TO_TARGET:
-			# direction from enemy -> target
-			enemy_actor.move_direction = enemy_actor.global_position.direction_to(target.global_position)
-			return SUCCESS
-
-	return FAILURE
+func get_class_name() -> Array[StringName]:
+	var classes := super()
+	classes.push_back(&"ActionLeaf")
+	return classes
