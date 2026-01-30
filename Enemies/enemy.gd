b8d282c6ba6a66f7dd8b4c9324307ced82620382
Wyatt <wyattschmidle@gmail.com>
2026-01-30 17:10:39 -0500

    progress with ai not done doing restart for plugin

---
+++ Enemies/enemy.gd
@@ -0,0 +1,27 @@
+class_name Enemy
+extends Node3D
+
+@export var health_bar: ProgressBar
+@export var max_health: int = 100
+@export var speed: float = 8.0
+
+var health: int = 100:
+	set(value):
+		health = value
+		if health_bar:
+			health_bar.value = health
+		if health <= 0:
+			queue_free()
+
+var move_direction: Vector3 = Vector3.ZERO
+
+func _ready() -> void:
+	health = max_health
+	if health_bar:
+		health_bar.max_value = max_health
+		health_bar.value = health
+
+	add_to_group("enemies")
+
+func _process(delta: float) -> void:
+	position += move_direction * speed * delta
